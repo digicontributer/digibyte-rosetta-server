956679eca168fcb36b6133dab2e30576e534365a
digicontributer <esot@digibyte.io>
2022-04-02 21:10:34 +0800

    update copyright headers

--- src/services/ConstructionService.js
+++ src/services/ConstructionService.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2020 DigiByte Foundation NZ Limited
+ * Copyright (c) 2020-2022 The DigiByte Core developers
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
